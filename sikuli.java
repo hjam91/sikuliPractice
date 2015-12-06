a630ccd2e779f2f1025716fba8c6cd00b0a771e3
Haroon Jameel <haroon.bc@gmail.com>
2015-12-06 14:14:25 -0500

    data provider

--- sikuli.java
+++ sikuli.java
@@ -8,6 +8,7 @@ import org.sikuli.script.FindFailed;
 import org.sikuli.script.Screen;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -34,21 +35,23 @@ public class sikuli {
             System.setProperty("webdriver.chrome.driver", prop.getProperty("chromeDriverLocation"));
             driver = new ChromeDriver();
         }
-        driver.manage().window().setPosition(new Point(0,0));
-        driver.manage().window().setSize(new Dimension(1200,1024));
-        driver.get(prop.getProperty("environment"));
+
 
     }
 
-        @Test
-        public void headerTest() {
+        @Test(dataProvider="getDimension")
+        public void headerTest(int x, int y, String size, String menu ) {
+
+            driver.manage().window().setPosition(new Point(0,0));
+            driver.manage().window().setSize(new Dimension(x,y));
+            driver.get(prop.getProperty("environment"));
 
             Screen s = new Screen();
             try {
 
                 Thread.sleep(2000);
-                File x1200 = new File(prop.getProperty("image1200"));
-               s.click(x1200);
+                File dimension = new File(size);
+                s.click(dimension);
 
             } catch (FindFailed e) {
                 e.printStackTrace();
@@ -78,6 +81,40 @@ public class sikuli {
                 fileInput.close();
                 return prop;
             }
+
+    @DataProvider
+    public Object[][] getDimension()
+    {
+        //Rows - Number of times your test has to be repeated.
+        //Columns - Number of parameters in test data.
+        Object[][] data = new Object[4][4];
+
+        // 1st row
+        data[0][0] = 1200;
+        data[0][1] =  800;
+        data[0][2] = prop.getProperty("image1200");
+        data[0][3] = prop.getProperty("image1200menu");
+
+        // 2nd row
+        data[1][0] = 1024;
+        data[1][1] =  600;
+        data[1][2] = prop.getProperty("image1024");
+        data[1][3] = prop.getProperty("image1024menu");
+
+        // 3rd row
+        data[2][0] = 960;
+        data[2][1] = 640;
+        data[2][2] = prop.getProperty("image960");
+        data[2][3] = prop.getProperty("image960menu");
+
+        data[3][0] = 480;
+        data[3][1] = 800;
+        data[3][2] = prop.getProperty("image480");
+        data[3][3] = prop.getProperty("image480menu");
+
+        return data;
+    }
+
 
         @AfterClass
         public static void breakDown(){
